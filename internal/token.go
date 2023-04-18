da8f43eaacbf5b3fd76761c377efa93fa24763c0
Preston Thompson <pwrtsoccer@gmail.com>
2023-04-17 19:05:38 -0500

    don't use form-url-encoding in the access token request
    required to work with Gong's OAuth2 implementation

--- internal/token.go
+++ internal/token.go
@@ -170,13 +170,12 @@ func newTokenRequest(tokenURL, clientID, clientSecret string, v url.Values, auth
 			v.Set("client_secret", clientSecret)
 		}
 	}
-	req, err := http.NewRequest("POST", tokenURL, strings.NewReader(v.Encode()))
+	req, err := http.NewRequest("POST", tokenURL+"?"+v.Encode(), strings.NewReader(""))
 	if err != nil {
 		return nil, err
 	}
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	if authStyle == AuthStyleInHeader {
-		req.SetBasicAuth(url.QueryEscape(clientID), url.QueryEscape(clientSecret))
+		req.SetBasicAuth(clientID, clientSecret)
 	}
 	return req, nil
 }
